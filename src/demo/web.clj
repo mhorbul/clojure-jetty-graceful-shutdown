96c830f72132ed96c9f83caeb2ab3d03c2229a9e
Mark Roghelia <mark.roghelia@livingsocial.com>
2012-10-23 13:36:38 -0400

    Use my own shutdown hook instead of jetty's

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -3,10 +3,10 @@
         ring.middleware.params)
   (:require [clojure.tools.logging :as log]))
 
-;(def jetty (atom nil))
+(def jetty (atom nil))
 
 (defn handler [{params :params}]
-  (. Thread (sleep 9000))
+  (. Thread (sleep 20000))
   {:status  200
    :headers {"Content-Type" "text/plain"}
    :body    (str "Hello " (params "name") "\n")})
@@ -14,17 +14,17 @@
 (def app
   (wrap-params handler))
 
-;(defn shutdown []
-  ;(log/info "Initiating shutdown...")
-  ; (.setShutdown @jetty true))  This isn't a method
-  ;(.stop @jetty)) ; This works if setGracefulShutdown is true, but isn't any better than setStopAtShutdown
+(defn shutdown []
+  (log/info "Initiating shutdown...")
+   ;(.setShutdown @jetty true))  This isn't a method
+  (.stop @jetty)) ; This works if setGracefulShutdown is true, but isn't any better than setStopAtShutdown
 
 (defn graceful-restart [jetty]
-    (.setGracefulShutdown jetty 10000)
-    (.setStopAtShutdown jetty true))
+    (.setGracefulShutdown jetty 100000))
+    ;(.setStopAtShutdown jetty true))
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
-    ;(.addShutdownHook (Runtime/getRuntime) (Thread. shutdown))
-    ;(reset! jetty (run-jetty app {:port port, :join? false :configurator graceful-restart}))))
-    (run-jetty app {:port port, :configurator graceful-restart})))
+    (.addShutdownHook (Runtime/getRuntime) (Thread. shutdown))
+    (reset! jetty (run-jetty app {:port port, :join? false :configurator graceful-restart}))))
+    ;(run-jetty app {:port port, :configurator graceful-restart})))
