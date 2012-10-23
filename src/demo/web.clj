714fe69963f1e5512f53b414103c1b6ef10e5ef6
Max Horbul <max@gorbul.net>
2012-10-22 18:24:42 -0700

    gracefully shutdown jetty

--- src/demo/web.clj
+++ src/demo/web.clj
@@ -1,13 +1,28 @@
 (ns demo.web
-  (:use ring.adapter.jetty))
+  (:use ring.adapter.jetty
+        ring.middleware.params)
+  (:require [clojure.tools.logging :as log]))
 
-(defn app [req]
-  (. Thread (sleep 10000))
-  {:status 200
+(def jetty (atom nil))
+
+(defn handler [{params :params}]
+  (. Thread (sleep 50000))
+  {:status  200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello from Clojure!\n"})
+   :body    (str "Hello " (params "name") "\n")})
+
+(def app
+  (wrap-params handler))
+
+(defn shutdown []
+ (log/info "Initiating shutdown...")
+ (.setShutdown @jetty true))
+
+(defn graceful-restart [jetty]
+    (.setGracefulShutdown jetty 55000)
+    (.setStopAtShutdown jetty true))
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
-    (run-jetty app {:port port})))
-
+    (.addShutdownHook (Runtime/getRuntime) (Thread. shutdown))
+    (reset! jetty (run-jetty app {:port port, :configurator graceful-restart}))))
